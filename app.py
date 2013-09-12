597dc545350999d900597e9c89ad8d68af4dcadf
hufeng <fengmi99@gmail.com>
2013-09-12 18:14:35 +0800

    salelog import

---
+++ app.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#
+# Using tornado to build our logs system.
+# Copyright(c) sales all rights reserved.
+
+import os
+import torndb
+import tornado.web
+import tornado.httpserver
+import tornado.ioloop
+
+from tornado.options import define, options
+define("port", default=3000, help="running on the given port", type=int)
+define("host", default="localhost:3306", help="db host")
+define("database", default="logs", help="database name")
+define("user", default="root", help="db username")
+define("password", default="root", help="db password")
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/', LoginHandler),
+            (r'/login', LoginHandler),
+            (r'/logout', AuthLogoutHandler),
+            (r'/home', HomeHandler),
+            (r'/log', LoggerHandler),
+            (r'/log/detail', LogDetailHandler),
+            (r'/logs', LoggerQueryHandler),
+            (r'/user', UserHandler),
+            (r'/user/(\d+)', UserDetailHandler),
+            (r'/user/(\w+)', UserHandler),
+            (r'/users', UserListHandler),
+            (r'/myself', MySelfHandler),
+            (r'/myself/password', MySelfHandler),
+        ]
+        settings = dict(
+            cookie_secret="61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+            login_url="/",
+            template_path=os.path.join(os.path.dirname(__file__), 'templates').replace("\\", "/"),
+            static_path=os.path.join(os.path.dirname(__file__), 'static').replace("\\", "/"),
+            debug=True
+        )
+        tornado.web.Application.__init__(self, handlers=handlers, **settings)
+        self.db = torndb.Connection(
+            host=options.host,
+            database=options.database,
+            user=options.user,
+            password=options.password
+        )
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.render('login.html')
+
+    def post(self):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        db = self.application.db
+        user = db.get("SELECT id, username, status FROM users WHERE username=%s and password=%s ", username, password)
+        print user
+        if user:
+            user_cookie = tornado.escape.json_encode(user)
+            self.set_secure_cookie("user", user_cookie)
+            self.write("success")
+        else:
+            self.write("fail")
+
+
+class AuthLogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("user")
+        self.redirect("/login")
+
+
+class HomeHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        user = tornado.escape.json_decode(self.get_secure_cookie("user"))
+        name = tornado.escape.xhtml_escape(user['username'])
+        status = tornado.escape.xhtml_escape(user['status'])
+        self.render("index.html", name=name, status=status)
+
+
+class UserHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.render("user.html")
+
+    def post(self, action):
+        db = self.application.db
+        username = self.get_argument("username")
+        password = self.get_argument("password")
+        status = self.get_argument("status")
+        if action == 'save':
+            user = db.get("SELECT username FROM users WHERE username = %s", username)
+            if user:
+                self.write("exist")
+                return
+            sql = "INSERT INTO users(username, password, status) VALUES(%s, %s, %s)"
+            db.execute(sql, username, password, status)
+            self.write("success")
+        elif action == 'update':
+            user_id = int(self.get_argument("id"))
+            sql = "UPDATE users SET username='%s', password='%s', status='%s' where id=%d" % (username, password, status, user_id)
+            db.execute(sql)
+            self.write("success")
+
+
+class UserDetailHandler(BaseHandler):
+    def get(self, user_id):
+        db = self.application.db
+        sql = "SELECT id, username, status FROM users WHERE id=%d" % int(user_id)
+        user = db.get(sql)
+        self.set_header("Content-Type", "application/json;charset=utf-8")
+        self.write(tornado.escape.json_encode(user))
+        self.finish()
+
+    def post(self, user_id):
+        db = self.application.db
+        sql = "DELETE FROM users where id = %d" % int(user_id)
+        db.execute(sql)
+        self.write("success")
+
+
+class UserListHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        page = int(self.get_argument("page"))
+        rows = int(self.get_argument("rows"))
+        start = (page -1) * rows
+        offset = start + rows
+        db = self.application.db
+        count = db.get("SELECT count(id) as count FROM users")
+        sql = "SELECT id, username, status FROM users ORDER BY id desc LIMIT %d, %d" % (start, offset)
+        users = db.query(sql)
+        self.set_header("Content-Type", "application/json;charset=utf-8")
+        self.write(tornado.escape.json_encode({'total': count["count"], "rows": users}))
+        self.finish()
+
+
+class MySelfHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        user = tornado.escape.json_decode(self.get_secure_cookie("user"))
+        self.render("myself.html", user=user)
+
+    def post(self):
+        user_id = self.get_argument("id")
+        pwd = self.get_argument("newPwd")
+        confirm = self.get_argument("confirmPwd")
+        if pwd is None or confirm is None:
+            self.write("null")
+        elif pwd != confirm:
+            self.write("notequal")
+        db = self.application.db
+        sql = "UPDATE users set password='%s' WHERE id=%d" % (pwd, int(user_id))
+        db.execute(sql)
+        self.write("success")
+
+
+class LoggerHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.render("log.html")
+
+
+class LoggerQueryHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        page = int(self.get_argument("page"))
+        rows = int(self.get_argument("rows"))
+        logtime=self.get_argument('logtime', None)
+        message = self.get_argument('message', None)
+        start = (page-1) * rows
+        offset = start + rows
+        db = self.application.db
+        count = db.get("SELECT count(*) as count FROM sales30_system_log")
+        sql = "SELECT id, logtime, message FROM sales30_system_log WHERE 1=1 "
+        if logtime:
+            sql += "logtime='%s' " % logtime
+        if message:
+            sql += " message like %'%s'%" % message
+        sql +=" ORDER BY logtime desc LIMIT %d, %d" % (start, offset)
+        print sql
+        rows = db.query(sql)
+        for log in rows:
+            log['logtime'] = log['logtime'].strftime("%Y-%m-%d %H:%M:%S")
+        self.set_header('Content-Type', "application/json;charset=utf-8")
+        self.write(tornado.escape.json_encode({"total": count['count'], "rows": rows}))
+        self.finish()
+
+class LogDetailHandler(BaseHandler):
+    def get(self):
+        log_id = int(self.get_argument('id'))
+        logtime = self.get_argument("logtime")
+        db = self.application.db
+        sql = "SELECT * FROM sales30_system_log WHERE id = %d and logtime='%s'" % (log_id, logtime)
+        print sql
+        row = db.get(sql)
+        self.render('log_detail.html', log=row)
+
+
+
+
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+
